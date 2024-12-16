f930aea1eaa8ee628fd23e8f31c180a3e63569e0
Anton Maksimovich <antonm@mutualart.com>
2024-12-16 21:29:24 +0300

    add index

--- src/main/java/com/example/freelanceplatformfordevs/config/SpringSecurity.java
+++ src/main/java/com/example/freelanceplatformfordevs/config/SpringSecurity.java
@@ -23,7 +23,8 @@ public class SpringSecurity {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf().disable()
                 .authorizeHttpRequests((authorize) -> authorize
-                        .anyRequest().permitAll()
+                        .requestMatchers("/", "../static/**",  "/index", "../img/**", "/register", "/login").permitAll()
+                        .anyRequest().authenticated()
                 )
                 .formLogin(form -> form
                         .loginPage("/login")
